badce4be51885425a4cf456e3cf00f96678278f8
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-12-29 16:47:28 +0800

    improving the texts on about

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -11,8 +11,22 @@ class _AboutScreenState extends State<AboutScreen> {
       appBar: AppBar(title: Text('About')),
       body: Column(
         children: [
-          Text(
-            'This is CRUD Application which was integrated with Laravel 10 API',
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'This is CRUD Application which was integrated with Laravel 10 API, well you can do it too',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 0), // space between texts
+                Text(
+                  'However this is just a beginning for me as a Fullstack developer so Wish Me Luck for this',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
           ),
         ],
       ),
